a95bf5fd6327abed29ad12431c9b5725f5486543
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-09-07 15:48:12 +0600

    add customer list page

---
+++ src/components/layout/vertical/VerticalMenu.jsx
@@ -0,0 +1,90 @@
+// MUI Imports
+import { useTheme } from '@mui/material/styles'
+
+// Third-party Imports
+import PerfectScrollbar from 'react-perfect-scrollbar'
+
+// Component Imports
+import { Menu, MenuItem, SubMenu } from '@menu/vertical-menu'
+
+// Hook Imports
+import useVerticalNav from '@menu/hooks/useVerticalNav'
+
+// Styled Component Imports
+import StyledVerticalNavExpandIcon from '@menu/styles/vertical/StyledVerticalNavExpandIcon'
+
+// Style Imports
+import menuItemStyles from '@core/styles/vertical/menuItemStyles'
+import menuSectionStyles from '@core/styles/vertical/menuSectionStyles'
+import CustomChip from '@/@core/components/mui/Chip'
+
+const RenderExpandIcon = ({ open, transitionDuration }) => (
+  <StyledVerticalNavExpandIcon open={open} transitionDuration={transitionDuration}>
+    <i className='tabler-chevron-right' />
+  </StyledVerticalNavExpandIcon>
+)
+
+const VerticalMenu = ({ scrollMenu }) => {
+  // Hooks
+  const theme = useTheme()
+  const verticalNavOptions = useVerticalNav()
+
+  // Vars
+  const { isBreakpointReached, transitionDuration } = verticalNavOptions
+  const ScrollWrapper = isBreakpointReached ? 'div' : PerfectScrollbar
+
+  return (
+    // eslint-disable-next-line lines-around-comment
+    /* Custom scrollbar instead of browser scroll, remove if you want browser scroll only */
+    <ScrollWrapper
+      {...(isBreakpointReached
+        ? {
+            className: 'bs-full overflow-y-auto overflow-x-hidden',
+            onScroll: container => scrollMenu(container, false)
+          }
+        : {
+            options: { wheelPropagation: false, suppressScrollX: true },
+            onScrollY: container => scrollMenu(container, true)
+          })}
+    >
+      {/* Incase you also want to scroll NavHeader to scroll with Vertical Menu, remove NavHeader from above and paste it below this comment */}
+      {/* Vertical Menu */}
+      <Menu
+        popoutMenuOffset={{ mainAxis: 23 }}
+        menuItemStyles={menuItemStyles(verticalNavOptions, theme)}
+        renderExpandIcon={({ open }) => <RenderExpandIcon open={open} transitionDuration={transitionDuration} />}
+        renderExpandedMenuItemIcon={{ icon: <i className='tabler-circle text-xs' /> }}
+        menuSectionStyles={menuSectionStyles(verticalNavOptions, theme)}
+      >
+        <MenuItem href='/dashboard' icon={<i className='tabler-smart-home' />}>
+          Dashboard
+        </MenuItem>
+
+        <SubMenu
+          label='Products'
+          icon={<i className='tabler-smart-home' />}
+          suffix={<CustomChip label='3' size='small' color='error' round='true' />}
+        >
+          <MenuItem href='/apps/products/list'>All Products</MenuItem>
+          <MenuItem href='/apps/products/add'>Add Product</MenuItem>
+          <MenuItem href='/apps/products/category'>Category</MenuItem>
+        </SubMenu>
+
+        <SubMenu label='Customers' icon={<i className='tabler-smart-home' />}>
+          <MenuItem href='/apps/customers/list'>All Customers</MenuItem>
+        </SubMenu>
+      </Menu>
+      {/* <Menu
+          popoutMenuOffset={{ mainAxis: 23 }}
+          menuItemStyles={menuItemStyles(verticalNavOptions, theme)}
+          renderExpandIcon={({ open }) => <RenderExpandIcon open={open} transitionDuration={transitionDuration} />}
+          renderExpandedMenuItemIcon={{ icon: <i className='tabler-circle text-xs' /> }}
+          menuSectionStyles={menuSectionStyles(verticalNavOptions, theme)}
+        >
+          <GenerateVerticalMenu menuData={menuData(dictionary)} />
+        </Menu> */}
+    </ScrollWrapper>
+  )
+}
+
+export default VerticalMenu
