9597fbacbd93b84484c42cc6c670b4a2726ded87
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-09-09 19:18:58 +0600

    add expired product page

--- src/app/(dashboard)/(private)/apps/stockList/expiredProducts/page.jsx
+++ src/app/(dashboard)/(private)/apps/stockList/expiredProducts/page.jsx
@@ -1,5 +1,12 @@
-const ExpiredProductsPage = () => {
-  return <h1>Expired Products page </h1>
+// Data Imports
+import { getEcommerceData } from '@/app/server/actions'
+import ExpiredProductList from '@/views/apps/ecommerce/stockList/expiredProducts/list'
+
+const ExpiredProductsPage = async () => {
+  // Vars
+  const data = await getEcommerceData()
+
+  return <ExpiredProductList expiredProducts={data?.expiredProducts} />
 }
 
 export default ExpiredProductsPage
