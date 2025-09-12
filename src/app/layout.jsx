0e94863168719ac2f27c7520133c53497519142d
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-09-12 23:35:43 +0600

    update: replace 'add logo image and text

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,7 @@ import '@/app/globals.css'
 import '@assets/iconify-icons/generated-icons.css'
 
 export const metadata = {
-  title: 'Vuexy - MUI Next.js Admin Dashboard Template',
+  title: 'Nexrox',
   description:
     'Vuexy - MUI Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
 }
