676efdb32375a50650251c4cc1ad17a0dd5a6912
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-11-16 18:50:28 +0600

    fix dynamic render error

--- src/app/(dashboard)/(private)/apps/profitLoss/page.jsx
+++ src/app/(dashboard)/(private)/apps/profitLoss/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { getProfitLoss } from '@/actions/incomeActions'
 import ProfitLoss from '@/views/apps/profitLoss'
 
--- src/app/(dashboard)/(private)/dashboard/page.jsx
+++ src/app/(dashboard)/(private)/dashboard/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { getAnalysisStats, getMonthlySummary } from '@/actions/dashboardActions'
 import DashboardClient from './DashboardClient'
 
